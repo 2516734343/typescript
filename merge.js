5240fe8c71dc0c5d25e5e443752e8ba32ae6b9cf
jingmeimei <jingmeimei@kuaishou.com>
2021-04-28 09:36:01 +0800

    泛型约束和多泛型使用

--- merge.js
+++ merge.js
@@ -16,37 +16,38 @@
 //         phone_number:132
 //     },
 // }
-    function transform(obj) {
-        let str = '';
-        for (const key in obj) {
-            if (typeof obj[key] != 'object') {
-                str = key;
-            } else {
-                str = key;
-                transform(obj[key]);
-            }
-        }
-        return str;
-    }
-    function change(obj) {
-        var result = str.replace(/[A-Z]/g,function (match) {
-                return "_"+match.toLowerCase();
-        })
-        if(result.splice(0,1) === '_'){
-            result = result.splice(1);
-        }
-        return result;
+// function transform(obj) {
+//     let str = '';
+//     for (const key in obj) {
+//         if (typeof obj[key] != 'object') {
+//             str = key;
+//         } else {
+//             str = key;
+//             transform(obj[key]);
+//         }
+//     }
+//     return str;
+// }
+// function change(obj) {
+//     var result = str.replace(/[A-Z]/g, function (match) {
+//         return "_" + match.toLowerCase();
+//     })
+//     if (result.splice(0, 1) === '_') {
+//         result = result.splice(1);
+//     }
+//     return result;
 
-    }
+// }
 
-    console.log(change(obj));
+// console.log(change(obj));
 
 
-var obj =  {
-    HappyPack:1,
-    UserName:1,
-    CompleteData:{
-        PhoneNumber:132
-    },
-}
-transform(obj);
+// var obj = {
+//     HappyPack: 1,
+//     UserName: 1,
+//     CompleteData: {
+//         PhoneNumber: 132
+//     },
+// }
+// transform(obj);
+
