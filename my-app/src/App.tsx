f99708cd60245adf440fbc2c2a14d8fa28da8a6c
2516734343 <2516734343@qq.com>
2021-11-27 10:06:12 +0800

    ts

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Game } from './components/Game';
+import { Game } from './chessGame/components/Game';
 function App() {
   return (
     <div className="App">
