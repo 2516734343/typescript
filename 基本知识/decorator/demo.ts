31b31df8a020f8bb610eddb92d5e6b3cda6b6db3
2516734343 <2516734343@qq.com>
2021-12-02 09:53:04 +0800

    装饰器对应的库

--- 基本知识/decorator/demo.ts
+++ 基本知识/decorator/demo.ts
@@ -1,14 +1,25 @@
 import { classDescriptor, printObj, propDescriptor } from "./Descriptor";
 
+import "reflect-metadata"
+import { IsNotEmpty, Max, MaxLength, Min, MinLength, validate } from "class-validator";
+import { Type } from "class-transformer";
 
 @classDescriptor('用户')
 class User {
 
   @propDescriptor('账号')
+  @IsNotEmpty({ message: '账号不可以为空' })
+  @MinLength(5, { message: '账号最少为5个字符' })
+  @MaxLength(12, { message: '账号最多为12个字符' })
   loginId: string
 
   @propDescriptor('密码')
   loginPwd: string
+
+  @Max(100, {message: '年龄最大为100'})
+  @Min(0, { message: '年龄最小为0' })
+  @Type(() => Number) // 运行时这个属性的类型
+  age: number;
 
 
 }
@@ -18,4 +29,23 @@ const u = new User();
 u.loginId = '123';
 u.loginPwd = '456';
 
+validate(u).then(error => {
+  console.log(error);
+})
+
 printObj(u)
+
+
+
+
+// 参数装饰器
+class MyMath {
+
+  sum(a: number, @test b: number) {
+
+  }
+}
+
+function test(target: any, method: string, index: number) {
+
+}
