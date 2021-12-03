05416ec976ad65372ee4c664ce13c5ecf7678e77
2516734343 <2516734343@qq.com>
2021-12-03 09:42:10 +0800

    声明文件

---
+++ 基本知识/声明文件/global.d.ts
@@ -0,0 +1,3 @@
+
+// 声明全局变量
+declare function setTimeout(handler: () => void, delay: number): null
---
+++ 基本知识/声明文件/lodash.d.ts
@@ -0,0 +1,6 @@
+
+// 声明模块
+
+declare module "lodash" {
+  export function chunk<T>(array: T[], size: number): T[][]
+}
